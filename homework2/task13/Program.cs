4427f1a6f07b1bc2e356a11ed910f1b5029ebbba
vik.stark <stavik17@ya.ru>
2023-07-09 17:07:42 +0300

    +hw2

---
+++ homework2/task13/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число от 1 до 100000: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if (99 < number && number < 1000)            // если число 3-значное
+{
+    int x1 = number % 10;
+    Console.WriteLine($"{x1} - третья цифра числа");
+}
+
+else if (999 < number && number < 10000)          // если число 4-значное
+{
+    int x1 = number % 10;
+    int x2 = (number % 100 - x1) / 10;
+    Console.WriteLine($"{x2} - третья цифра числа");
+}
+
+else if (9999 < number && number < 100000)        // если число 5-значное
+{
+    int x1 = number % 10;
+    int x2 = (number % 100 - x1) / 10;
+    int x3 = (number % 1000 - x2) / 100;
+    Console.WriteLine($"{x3} - третья цифра числа");
+}
+
+else if (number > 100000)                         // если число 6-значное и выше
+{
+    Console.Write("Слишком длинное число для данной программы. Попробуйте снова.");
+}
---
+++ homework2/task15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите номер дня недели, чтобы проверить является ли этот день выходным: ");
+string? Days = Console.ReadLine();
+int IntDays = Convert.ToInt32(Days);
+
+if (IntDays == 1) Console.WriteLine ("Нет, это не выходной");
+if (IntDays == 2) Console.WriteLine ("Нет, это не выходной");
+if (IntDays == 3) Console.WriteLine ("Нет, это не выходной");
+if (IntDays == 4) Console.WriteLine ("Нет, это не выходной");
+if (IntDays == 5) Console.WriteLine ("Нет, это не выходной");
+if (IntDays == 6) Console.WriteLine ("Да, это выходной!");
+if (IntDays == 7) Console.WriteLine ("Да, это выходной!");
+if (IntDays > 7 || IntDays < 1) Console.WriteLine("Нет такого дня, ты не с этой планеты, парень!");
