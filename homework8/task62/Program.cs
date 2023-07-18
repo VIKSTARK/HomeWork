151721ea06b099f8fdaf7f81cb2b23749cfd42ba
vik.stark <stavik17@ya.ru>
2023-07-18 17:12:50 +0300

    +hw8

--- homework8/task62/Program.cs
+++ homework8/task62/Program.cs
@@ -5,3 +5,36 @@
 // 11 16 15 06
 // 10 09 08 07
 
+const int rows = 4;
+const int columns = 4;
+
+int[,] matrix = new int[rows, columns];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+    matrix[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < matrix.GetLength(1) - 1) j++;
+    else if (i < j && i + j >= matrix.GetLength(0) - 1) i++;
+    else if (i >= j && i + j > matrix.GetLength(1) - 1) j--;
+    else i--;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] / 10 <= 0) Console.Write($" {array[i, j]} ");
+            else Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(matrix);
