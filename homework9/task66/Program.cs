a002ff6f6592d76ffac8c96cbf82be1298a8e9f3
vik.stark <stavik17@ya.ru>
2023-07-20 23:51:31 +0300

    +hw9

--- homework9/task66/Program.cs
+++ homework9/task66/Program.cs
@@ -2,3 +2,15 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.WriteLine("Введите первое натуральное число M");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число N");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int sumOfNaturalElements(int N, int M)
+{
+    if (M == N) return M;
+    return N + sumOfNaturalElements(N + 1, M);
+}
+
+Console.WriteLine($"Сумма чисел от {N} до {M} равна: {sumOfNaturalElements(N, M)}");
--- homework9/task68/Program.cs
+++ homework9/task68/Program.cs
@@ -3,3 +3,16 @@
 // m = 3, n = 2 -> A(m,n) = 9
 // m = 2, n = 3 -> A(m,n) = 29
 
+Console.WriteLine("Введите первое натуральное число m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int AckermanFunction(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (m == 0) return AckermanFunction(n - 1, 1);
+    return AckermanFunction(n - 1, AckermanFunction(n, m - 1));
+}
+
+Console.WriteLine(AckermanFunction(n, m));
