772559ff54c81dd47364878e0a22f9de03729f90
vik.stark <stavik17@ya.ru>
2023-07-21 00:08:59 +0300

    +hw9

--- homework9/task64/Program.cs
+++ homework9/task64/Program.cs
@@ -3,3 +3,14 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+Console.WriteLine("Введите натуральное число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int NaturalNumbers(int n, int m)
+{
+  if (n == m) return n;
+  else Console.Write($"{NaturalNumbers(n, m + 1)}, ");
+  return m;
+}
+
+Console.WriteLine(NaturalNumbers(n, 1));
